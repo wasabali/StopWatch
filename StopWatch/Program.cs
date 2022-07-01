e7fe3aecf86d42d3b25282dadefeae261b1d8cd7
Wasab Ali <wasabali@MacBook-Pro.local>
2022-07-01 12:52:40 +0200

    first commit

---
+++ StopWatch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace StopWatch
+{
+    public partial class Program
+	{
+
+        public static void Main (string[] args)
+        {
+            var myWatch = new StopWatch();
+           
+            Console.WriteLine("press spacebar to start");
+
+            while (true)
+            {
+                if ((!myWatch.GetWatchActive()) && (Console.ReadKey().Key == ConsoleKey.Spacebar))
+                {
+                    myWatch.Start();
+                    Console.WriteLine("staring stopwatch...");
+                }
+                
+                if ((myWatch.GetWatchActive()) && (Console.ReadKey().Key == ConsoleKey.Spacebar))
+                {
+                    myWatch.Stop();
+                    Console.WriteLine("stopping stopwatch...");
+                    Console.WriteLine("duration: " + myWatch.Stop());
+                }
+            }
+
+        }
+	}
+}
+
---
+++ StopWatch/StopWatch.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace StopWatch
+{
+    public partial class Program
+	{
+        public class StopWatch
+        {
+            private TimeSpan _duration;
+            private DateTime _startTime;
+            private DateTime _endTime;
+            private bool watchActive = false;
+
+            //public StopWatch ()
+            //{
+            //}
+
+            //public StopWatch (bool watchActive)
+            //    : this()
+            //{
+            //    this.watchActive = watchActive;
+            //}
+
+            public void SetWatchActive (bool active)
+            {
+                watchActive = active;
+            }
+
+            public bool GetWatchActive ()
+            {
+                return watchActive;
+            }
+
+            public void Start()
+            {
+                if (!watchActive)
+                {
+                    _startTime = DateTime.Now;
+                    watchActive = true;
+                }
+                else
+                {
+                    throw new Exception("Stopwatch already running");
+                }
+            }
+
+            public string Stop()
+            {
+                if (watchActive)
+                {
+                    _endTime = DateTime.Now;
+                    _duration = _endTime - _startTime;
+                    watchActive = false;
+                }
+                    return _duration.ToString();
+            }
+        }
+	}
+}
+
